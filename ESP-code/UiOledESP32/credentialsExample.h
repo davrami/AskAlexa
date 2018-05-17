e3ad26ea91fc45a4edce8bd672263f817c4e707d
davrami <david.rami007@gmail.com>
2018-05-17 20:01:44 +0200

    example credentials

---
+++ ESP-code/UiOledESP32/credentialsExample.h
@@ -0,0 +1,18 @@
+//const char* ssid     = "yourSSID";
+//const char* password = "yourPassword";
+
+//const char* ssid     = "";
+//const char* password = "";
+
+const char* ssid     = "";
+const char* password = "";
+
+
+//const char* ssid = "";
+//const char* password = "";
+
+
+const char* mqtt_server = "";
+int mqtt_port = 1883;
+const char* mqtt_username = "";
+const char* mqtt_password = "";
